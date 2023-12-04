69a49c05d8e4ef7748e46378413804a2a0ab7ec4
Matt Woodside <bmwoodside@live.com>
2023-12-03 23:09:54 -0500

    day1 part1 completion - now for part2 xD

--- AoC2023/day1.js
+++ AoC2023/day1.js
@@ -28,15 +28,17 @@ fs.readFile('./day1input.txt','utf-8',(err, data) => {
         // if (a && !b) { // this is redundant as long as the second if-statement doesn't get modified in the future.
         //     b = a;
         // }
-        countTotal += (a + b);
+        let text = (`${a.toString()}` + `${b.toString()}`)
+        countTotal += Number(text);
     }
 
     console.log(countTotal);
 
-    return countTotal // something wrong happening - returns 9985, but there are 10k lines in day1input - number should output much higher value.
+    return countTotal
 
 })
 
+// day1
 // prompt1
 //The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
 // For example:
@@ -46,35 +48,6 @@ fs.readFile('./day1input.txt','utf-8',(err, data) => {
 // treb7uchet
 // In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 // Consider your entire calibration document. What is the sum of all of the calibration values?
-
-// function sumTextValues(input) {
-//     let countTotal = 0;
-
-    // for (let i = 0; i < input.length; i++) { // loop for rows
-    //     let a; // value of first number found
-    //     let b; // running value of last number seen
-    
-    //     for (let j = 0; j < input[i].length; j++) { // loop for-each row as a string
-    //         if (a == undefined && parseInt(input[i][j]) ) {
-    //             a = Number(input[i][j]);
-    //         }
-
-    //         if (parseInt(input[i][j])) {
-    //             b = Number(input[i][j]);
-    //         }
-    //     }
-
-    //     // if (a && !b) { // this is redundant as long as the second if-statement doesn't get modified in the future.
-    //     //     b = a;
-    //     // }
-    //     countTotal += (a + b);
-    // }
-
-    // console.log(countTotal);
-    // return countTotal;
-// }
-
-// const tempTestArr = ["1abc2", "pqr3stu8vwx", "abc3cba"];
-// sumTextValues(tempTestArr);
-
-// sumTextValues(tempTextValues);
+// prompt2
+// Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+// Equipped with this new information, you now need to find the real first and last digit on each line.
